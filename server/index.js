f4478d02c9b212c7046a6b7b24999b694d518348
luizahieger <luiza_hieger-silva@estudante.sesisenai.org.br>
2024-10-31 12:13:25 -0300

    teste

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
-import Express from 'express'
+import Express from "express";
+import { criarTabelas, User } from "./db.js";
+import bcryptjs from "bcryptjs"
 
 const app = Express()
+app.use(Express.json())
 
 
-app.get('/pegar', function (req, res) {
-    res.send('enviar esta mensagem')
+app.post('/registro', async function (req, res) {
+    try {
+        const{nome, sobrenome, email, senha, dataNascimento} = req.body
+        if (!nome || !sobrenome || !email || !senha || !dataNascimento){
+            res.status(406).send('Todos os campos devem ser preenchidos.')
+            return
+        }
+        if(await User.findOne ({where:{email:email}})){
+            res.status(400).send('Usuário já existente no sistema!')
+            return 
+        }
+        const senhaSegura = bcryptjs.hashSync(senha, 10)
+        const novoUsuario = User.create({
+            nome: nome,
+            sobrenome: sobrenome,
+            email: email, 
+            senha: senhaSegura,
+            dataNasc: dataNascimento
+        })
+        res.status(201).send('Ok! Usuário criado.')
+    } catch (erro) {
+        console.log(erro)
+    }
 })
 
-app.get('/pegaroutracoisa', function (req, res){
-    res.send('esta é outra mensagem')
-})
-
-
-app.listen(8000)
